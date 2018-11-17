cc01eec7a536650191de2e9c4dd91cf60071b242
Victor Mira <coding.reakktor@gmail.com>
2018-11-17 15:39:56 -0300

    change bg color for PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,8 +12,8 @@ module.exports = {
         name: 'VMMIRA_Desarrollo_Web',
         short_name: 'VMMIRA',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#2A2F4A',
+        theme_color: '#2A2F4A',
         display: 'minimal-ui',
         icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
       },
